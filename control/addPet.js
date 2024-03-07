a6e74b320234a66db8028c64b2c873fd3210692e
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-03-07 11:01:33 -0300

    feat: add the function  delete card

--- control/addPet.js
+++ control/addPet.js
@@ -20,20 +20,30 @@ buttonSend.addEventListener('click', (e) => {
     const state = document.getElementById("istate")
     const city = document.getElementById("icity")
     const photo = document.getElementById("iphoto").files[0]
-    const gender = document.getElementsByName("gender")[0]
+    const gender = document.querySelectorAll('input[name="gender"]')
     if(race.value != '' && size.value != '' && state.value != '' && city.value != '' && gender.value != '' && photo.value != '') {
         e.preventDefault()
         modal.close()
         document.body.style.overflow = ''
 
+        let genderSelected
+
+        gender.forEach(radio => {
+            if (radio.checked) {
+                genderSelected = radio.value
+            }
+         })
+
         const reader = new FileReader(); 
 
         reader.onload = function(event) {
             const photoURL = event.target.result; 
-            addNewPet(race.value, gender.value, size.value, state.value, city.value, photoURL); 
+            addNewPet(race.value, genderSelected, size.value, state.value, city.value, photoURL); 
         };
 
         reader.readAsDataURL(photo); 
+
+
     }   
 })
 
@@ -60,58 +70,22 @@ function addNewPet (race,gender,size,state,city, photo) {
 }
 
 window.onload = () => {
-    console.log(localStorage.length)
     if (localStorage.length > 0 ) {
         filter.style.display = 'block'
-        
-    }
-    for (var i = 0; i <= localStorage.length; i++) {
-        let chave = localStorage.key(i)
-        let petsAdded = localStorage.getItem(chave)
 
-        let pet = JSON.parse(petsAdded)
-        let div = document.createElement("div")
-        div.classList.add("card")
+        for (var i = 0; i < localStorage.length; i++) {
+            let chave = localStorage.key(i)
+            let petsAdded = localStorage.getItem(chave)
     
-        const race = document.createElement("span")
-        race.innerHTML = pet.race
-    
-        const gender = document.createElement("span")
-        gender.innerHTML = pet.gender
-    
-        const size = document.createElement("span")
-        size.innerHTML = pet.size
-    
-        const state = document.createElement("span")
-        state.innerHTML = pet.state
-    
-        const city = document.createElement("span")
-        city.innerHTML = pet.city
-    
-        const image = document.createElement("img");
-        image.src = pet.photo; // Use a URL da foto diretamente
-        image.style.maxWidth = '250px'
-        image.style.maxHeight = '180px'
-    
-    
-        sectionPets.appendChild(div)
-        div.appendChild(image)
-        div.appendChild(race)
-        div.appendChild(gender)
-        div.appendChild(size)
-        div.appendChild(state)
-        div.appendChild(city)
+            let pet = JSON.parse(petsAdded)
+
+            createElementsCard(pet, i)
+        }
     }
+    
 }
 
-
-function createNewCard () {
-
-    filter.style.display = 'block'
-    let lastPetAdded = petAvailable[petAvailable.length - 1]
-
-    localStorage.setItem(`Pet${petAvailable.length-1}`,JSON.stringify(lastPetAdded))
-
+function createElementsCard(lastPetAdded, id) {
     let div = document.createElement("div")
     div.classList.add("card")
     
@@ -131,9 +105,17 @@ function createNewCard () {
     city.innerHTML = lastPetAdded.city
 
     const image = document.createElement("img");
-    image.src = lastPetAdded.photo; // Use a URL da foto diretamente
+    image.src = lastPetAdded.photo; 
     image.style.maxWidth = '250px'
-    image.style.maxHeight = '180px'
+    image.style.maxHeight = '100px'
+
+    const button = document.createElement("button")
+    button.innerHTML = 'Quero Adotar'
+    button.classList.add("buttonAdote")
+
+    button.addEventListener('click', function() {
+        wantAdopt(div, id)
+    })
 
     sectionPets.appendChild(div)
     div.appendChild(image)
@@ -142,7 +124,21 @@ function createNewCard () {
     div.appendChild(size)
     div.appendChild(state)
     div.appendChild(city)
+    div.appendChild(button)
+}
 
+function wantAdopt(div,id) {
+    localStorage.removeItem(id)
+    sectionPets.removeChild(div)
+}
 
+function createNewCard () {
+
+    filter.style.display = 'block'
+    let lastPetAdded = petAvailable[petAvailable.length - 1]
+    let id = petAvailable.length-1
+
+    localStorage.setItem(id, JSON.stringify(lastPetAdded))
+    createElementsCard(lastPetAdded, id)
 }
 
